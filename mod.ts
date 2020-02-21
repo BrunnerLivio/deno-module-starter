ba7f8b5e54fb6c5210aadba31a674da7b02a9db0
Livio <livio.brunner.lb1@gmail.com>
2020-02-21 23:18:17 +0100

    Fix formatting

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
-import { bold } from './deps.ts';
+import { bold } from "./deps.ts";
 
 /** Returns `Hello World` in bold */
 export function getHelloWorld(): string {
-  return bold('Hello World');
+  return bold("Hello World");
 }
