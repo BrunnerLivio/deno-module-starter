a6b3a3e4234da4b3db63fbcf691388dbe047029a
bidetaggle <bidetaggle@mac>
2021-01-06 15:51:25 +0100

    update version in test_deps.ts

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
 // Add your test dependencies in here
-export { assertEquals } from "https://deno.land/std@v0.32.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@v0.83.0/testing/asserts.ts";
